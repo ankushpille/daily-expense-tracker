535f500722e78eeaa1eac94389dae787f30ea790
ankushpille <ankush.pille1@gmail.com>
2026-02-08 12:57:53 +0530

    refactor

--- vite-project/src/ExpenseForm.jsx
+++ vite-project/src/ExpenseForm.jsx
@@ -1,70 +1,84 @@
 import { useState } from "react";
 
 export default function ExpenseForm() {
-  const [amount, setAmount] = useState(0);
-  const [category, setCategory] = useState([
-    "food",
-    "petrol",
-    "groceries",
-    "recharge",
-    "wifi",
-    "home maintainence bill",
-    "Power bill",
-    "others",
-  ]);
-  const [date, setDate] = useState(null);
+  const [amount, setAmount] = useState("");
+  const [category, setCategory] = useState("");
+  const [date, setDate] = useState(new Date().toISOString().split("T")[0]);
   const [description, setDescription] = useState("");
-  const [payementMode, setPayementMode] = useState([
-    "phonepe",
-    "gpay",
-    "paytym",
-    "creditcard",
-    "cash",
-    "others",
-  ]);
+  const [payementMode, setPayementMode] = useState("");
 
+  const handleAmountOnChange = (e) => {
+    setAmount(e.target.value);
+  };
+
+  const handleDescriptionChange = (e) => {
+    setDescription(e.target.value);
+  };
+
+  const handleDateChange = (e) => {
+    setDate(e.target.value);
+  };
+
+  const handleCategoryOnChange = (e) => {
+    setCategory(e.target.value);
+  };
+
+  const handlePayementChange = (e) => {
+    setPayementMode(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
     <>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label>
           Enter Your amount
-          <input type="text" value={amount} />
+          <input type="text" value={amount} onChange={handleAmountOnChange} />
         </label>{" "}
         <br></br>
         <label>
           Category:
-          <select>
-            <option>{category[0]}</option>
-            <option>{category[1]}</option>
-            <option>{category[2]}</option>
-            <option>{category[3]}</option>
-            <option>{category[4]}</option>
-            <option>{category[5]}</option>
-            <option>{category[6]}</option>
+          <select value={category} onChange={handleCategoryOnChange}>
+            <option value="">Select Category</option>
+            <option value="Food">Food</option>
+            <option value="Travel">Travel</option>
+            <option value="Rent">Rent</option>
+            <option value="Shopping">Shopping</option>
+            <option value="Bills">Bills</option>
+            <option value="Others">Others</option>
           </select>
         </label>{" "}
         <br />
         <label>
           Enter your date:
-          <input type="date" value={date} />
+          <input type="date" value={date} onChange={handleDateChange} />
         </label>
         <br />
         <label>
           description:
-          <input type="text" value={description} />
+          <input
+            type="text"
+            value={description}
+            onChange={handleDescriptionChange}
+          />
         </label>
         <br />
         <label>
           Select payement mode:
-          <select>
-            <option>{payementMode[0]}</option>
-            <option>{payementMode[1]}</option>
-            <option>{payementMode[2]}</option>
-            <option>{payementMode[3]}</option>
-            <option>{payementMode[4]}</option>
-            <option>{payementMode[5]}</option>
+          <select value={payementMode} onChange={handlePayementChange}>
+            <option value="payement type">payement type</option>
+            <option value="phonepe">Phonepe</option>
+            <option value="gpay">gpay</option>
+            <option value="paytym">paytym</option>
+            <option value="creditcard">creditcard</option>
+            <option value="debitcard">debitcard</option>
+            <option value="others">others</option>
           </select>
         </label>
+        <br />
+        <button type="submit">Add expense</button>
       </form>
     </>
   );
